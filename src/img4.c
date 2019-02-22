9db1056e311e17fb4c85ee0a602efc9c439fa9ca
s0uthwest <pashok19967@yandex.ru>
2019-02-22 08:47:32 -0800

    fix broken function "waiting for device..."

--- src/img4.c
+++ src/img4.c
@@ -123,8 +123,7 @@ static const unsigned char *asn1_find_element(unsigned int index, unsigned char 
 	}
 
 	// find the element we are searching
-    int i = 0;
-    while (i <= index, i++) {
+	for (int i = 0; i <= index; i++) {
 		off += asn1_get_element(&data[off], &el_type, &el_size);
 		if (i == index)
 			break;
